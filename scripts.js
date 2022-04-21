fc52ffaec79030a35b21ba85ed7f0a467be6b7d0
Sergio Trajano <sergio.trajanosilva2001@gmail.com>
2022-04-21 13:49:08 -0300

    Quizzes do Usuário: Armazenar quizzes do usuário

--- scripts.js
+++ scripts.js
@@ -1,4 +1,169 @@
-function perguntasValidas() {
+
+function tratarErro() {
+    alert("Houve algum erro no processo de salvamento!");
+}
+
+function concluirCriacao(response) {
+    let userId = localStorage.getItem("ids");
+    userId = JSON.parse(userId);
+    userId[userId.length] = response.data.id;
+    userId = JSON.stringify(userId);
+    localStorage.removeItem("ids");
+    localStorage.setItem("ids", userId);
+    const img = document.querySelector(".finalizado img");
+    img.src = document.querySelector(".informacoes-basicas input:nth-child(2)").value;
+    document.querySelector(".niveis").classList.add("escondido");
+    document.querySelector(".finalizado").classList.remove("escondido");
+}
+
+function acharNivel(i) {
+    const nivel = document.querySelector(`.niveis li:nth-child(${i})`);
+    return {
+        title: nivel.querySelector("input:nth-child(1)").value,
+        image: nivel.querySelector("input:nth-child(3)").value,
+        text: nivel.querySelector("input:nth-child(4)").value,
+        minValue: nivel.querySelector("input:nth-child(2)").value
+    }
+}
+
+function retornarNiveis() {
+    const levels = [];
+    const numNiveis = parseInt(document.querySelector(".informacoes-basicas input:last-child").value);
+    for (let i = 1; i <= numNiveis; i++) {
+        levels[i-1] = acharNivel(i);
+    }
+    return levels;
+}
+
+function acharPergunta(i) {
+    const pergunta = document.querySelector(`.perguntas li:nth-child(${i})`);
+    const titulo = pergunta.querySelector(".maximizado input:nth-child(1)").value;
+    const cor = pergunta.querySelector(".maximizado input:nth-child(2)").value;
+    const opcoes = [{
+        text: pergunta.querySelector(".maximizado input:nth-child(4)").value,
+        image: pergunta.querySelector(".maximizado input:nth-child(5)").value,
+        isCorrectAnswer: true
+    }];
+    for (let j = 7; j < 12; j+=2) {
+        if (pergunta.querySelector(`.maximizado input:nth-child(${j})`).value !== "") {
+            opcoes[opcoes.length] = {
+                text: pergunta.querySelector(`.maximizado input:nth-child(${j})`).value,
+                image: pergunta.querySelector(`.maximizado input:nth-child(${j+1})`).value,
+                isCorrectAnswer: false
+            }
+        }
+    }
+    return {
+        title: titulo,
+        color: cor,
+        answers: opcoes
+    }
+}
+
+function retornarPerguntas() {
+    const questions = []
+    const numPerguntas = parseInt(document.querySelector(".informacoes-basicas input:nth-child(3)").value);
+    for (let i = 1; i <= numPerguntas; i++) {
+        questions[i-1] = acharPergunta(i);
+    }
+    return questions;
+}
+
+function retornarUrl() {
+    return document.querySelector(".informacoes-basicas input:nth-child(2)").value;
+}
+
+function retornarTitulo() {
+    return document.querySelector(".informacoes-basicas input:first-child").value;
+}
+
+function validarNiveis() {
+    const numNiveis = parseInt(document.querySelector(".informacoes-basicas input:last-child").value);
+    const regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    let tituloNivel;
+    let porcent;
+    let url;
+    let descricao;
+    let porcetZero = false;
+    for (let i = 1; i <= numNiveis; i++) {
+        tituloNivel = document.querySelector(`.niveis li:nth-child(${i}) input:nth-child(1)`).value;
+        porcent = parseInt(document.querySelector(`.niveis li:nth-child(${i}) input:nth-child(2)`).value);
+        url = document.querySelector(`.niveis li:nth-child(${i}) input:nth-child(3)`).value;
+        descricao = document.querySelector(`.niveis li:nth-child(${i}) input:nth-child(4)`).value;
+        if (tituloNivel.length < 10) {
+            return false;
+        }
+        if (porcent < 0 || porcent > 100) {
+            return false;
+        }
+        if (porcent === 0) {
+            porcetZero = true;
+        }
+        if (!regexp.test(url)) {
+            return false;
+        }
+        if (descricao.length < 30) {
+            return false;
+        }
+    }
+    if (!porcetZero) {
+        return false
+    }
+    return true;
+}
+
+function salvarQuizz() {
+    if (validarNiveis()) {
+        const quizz = {
+            title: retornarTitulo(),
+            image: retornarUrl(),
+            questions: retornarPerguntas(),
+            levels: retornarNiveis()
+        };
+        const promisse = axios.post("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes", quizz);
+        promisse.then(concluirCriacao);
+        promisse.catch(tratarErro);
+    } else {
+        alert("Há algum dado inválido!");
+    }
+}
+
+function preencherNiveis() {
+    const listaNiveis = document.querySelector(".niveis ul");
+    const numNiveis = parseInt(document.querySelector(".informacoes-basicas input:last-child").value);
+    listaNiveis.innerHTML = "";
+    for (let i = 0; i < numNiveis; i++) {
+        if (i === 0) {
+            listaNiveis.innerHTML += `
+                <li class="selecionado">
+                    <p>Nivel 1</p>
+                    <img class="icone escondido" onclick="maximizarPergunta(this, 'niveis')" src="Vector.png" alt="" style="width: 26px; height: 23px;">
+                    <div class="maximizado">
+                        <input type="text" placeholder="Título do nível">
+                        <input type="text" placeholder="% de acerto mínima">
+                        <input type="text" placeholder="URL da imagem do nível">
+                        <input type="text" placeholder="Descrição do nível">
+                    </div>
+                </li>
+            `
+        } else {
+            listaNiveis.innerHTML += `
+                <li>
+                    <p>Nivel ${i+1}</p>
+                    <img class="icone" onclick="maximizarPergunta(this, 'niveis')" src="Vector.png" alt="" style="width: 26px; height: 23px;">
+                    <div class="maximizado escondido">
+                        <input type="text" placeholder="Título do nível">
+                        <input type="text" placeholder="% de acerto mínima">
+                        <input type="text" placeholder="URL da imagem do nível">
+                        <input type="text" placeholder="Descrição do nível">
+                    </div>
+                </li>
+            `
+        }
+    }
+}
+
+function validarPerguntas() {
     const regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
     let textoPergunta;
     let corFundo;
@@ -6,7 +171,7 @@ function perguntasValidas() {
     let urlResposta;
     let textoErro;
     let urlErro;
-    const numPerguntas = Number(document.querySelector(".informacoes-basicas input:nth-child(3)").value);
+    const numPerguntas = parseInt(document.querySelector(".informacoes-basicas input:nth-child(3)").value);
     for (let i = 1; i <= numPerguntas; i++) {
         textoPergunta = document.querySelector(`.perguntas li:nth-child(${i}) .maximizado input:nth-child(1)`).value;
         corFundo = document.querySelector(`.perguntas li:nth-child(${i}) .maximizado input:nth-child(2)`).value.toUpperCase();
@@ -19,7 +184,7 @@ function perguntasValidas() {
             return false;
         }
         for (let k = 1; k < 7; k++) {
-            if (corFundo[k] !== 'A' && corFundo[k] !== 'B' && corFundo[k] !== 'C' && corFundo[k] !== 'D' && corFundo[k] !== 'E' && corFundo[k] !== 'F') {
+            if (corFundo[k] !== 'A' && corFundo[k] !== 'B' && corFundo[k] !== 'C' && corFundo[k] !== 'D' && corFundo[k] !== 'E' && corFundo[k] !== 'F' && isNaN(Number(corFundo[k]))) {
                 return false;
             }
         }
@@ -39,22 +204,26 @@ function perguntasValidas() {
                 return false;
             }
         }
+        if (document.querySelector(`.perguntas li:nth-child(${i}) .maximizado input:nth-child(7)`).value === "" && document.querySelector(`.perguntas li:nth-child(${i}) .maximizado input:nth-child(9)`).value === "" && document.querySelector(`.perguntas li:nth-child(${i}) .maximizado input:nth-child(11)`).value === "") {
+            return false;
+        }
     }
     return true;
 }
 
-function verificaFormPerguntas() {
-    if (perguntasValidas()) {
+function verificarPerguntas() {
+    if (validarPerguntas()) {
         document.querySelector(".perguntas").classList.add("escondido");
-        document.querySelector(".niveis").remove("escondido");
+        document.querySelector(".niveis").classList.remove("escondido");
+        preencherNiveis();
     } else {
         alert("Há alguma informação invalida!");
     }
 }
 
-function maximizaPerguntas(elemento) {
+function maximizarPergunta(elemento, fase) {
     const maximizar = elemento.parentNode.querySelector(".maximizado");
-    const minimizar = document.querySelector(".perguntas .selecionado");
+    const minimizar = document.querySelector(`.${fase} .selecionado`);
     if (minimizar !== null) {
         minimizar.querySelector(".maximizado").classList.add("escondido");
         minimizar.querySelector("img").classList.remove("escondido");
@@ -68,14 +237,14 @@ function maximizaPerguntas(elemento) {
 
 function preencherPerguntas() {
     const formularioPerguntas = document.querySelector(".perguntas ul");
-    const numPerguntas = Number(document.querySelector(".dados-basicos input:nth-child(3)").value);
+    const numPerguntas = parseInt(document.querySelector(".dados-basicos input:nth-child(3)").value);
     formularioPerguntas.innerHTML = "";
     for (let i = 0; i < numPerguntas; i++) {
         if (i === 0) {
             formularioPerguntas.innerHTML += `
             <li class="selecionado">
                 <p>Pergunta 1</p>
-                <img class="icone escondido" onclick="maximizaPerguntas(this)" src="Vector.png" alt="" style="width: 26px; height: 23px;">
+                <img class="icone escondido" onclick="maximizarPergunta(this, 'perguntas')" src="Vector.png" alt="" style="width: 26px; height: 23px;">
                 <div class="maximizado">
                     <input type="text" placeholder="Texto da pergunta">
                     <input type="text" placeholder="Cor de fundo da pergunta">
@@ -96,7 +265,7 @@ function preencherPerguntas() {
             formularioPerguntas.innerHTML += `
                 <li>
                     <p>Pergunta ${i+1}</p>
-                    <img class="icone" onclick="maximizaPerguntas(this)" src="Vector.png" alt="" style="width: 26px; height: 23px;">
+                    <img class="icone" onclick="maximizarPergunta(this, 'perguntas')" src="Vector.png" alt="" style="width: 26px; height: 23px;">
                     <div class="maximizado escondido">
                         <input type="text" placeholder="Texto da pergunta">
                         <input type="text" placeholder="Cor de fundo da pergunta">
@@ -122,16 +291,16 @@ function informacoesValidas(titulo, url, numPerguntas, numNiveis) {
     if (titulo.length < 20 || titulo.length > 65) {
         return false;
     }
-    if (Number(numPerguntas) < 3) {
+    if (parseInt(numPerguntas) < 3) {
         return false;
     }
-    if (Number(numNiveis) < 2) {
+    if (parseInt(numNiveis) < 2) {
         return false;
     }
     return regexp.test(url);
 }
 
-function verificaInformacoesBasicas() {
+function verificarInfoBasicas() {
     const titulo = document.querySelector(".informacoes-basicas input:nth-child(1)").value;
     const url = document.querySelector(".informacoes-basicas input:nth-child(2)").value;
     const numPerguntas = document.querySelector(".informacoes-basicas input:nth-child(3)").value;
@@ -139,6 +308,7 @@ function verificaInformacoesBasicas() {
     if (informacoesValidas(titulo, url, numPerguntas, numNiveis)) {
         document.querySelector(".informacoes-basicas").classList.add("escondido");
         document.querySelector(".perguntas").classList.remove("escondido");
+        preencherPerguntas();
     } else {
         alert("Alguma informação é invalida!!")
     }
@@ -146,10 +316,12 @@ function verificaInformacoesBasicas() {
 
 function prosseguir(fase) {
     if (fase === "pergunta") {
-        preencherPerguntas();
-        verificaInformacoesBasicas();
+        verificarInfoBasicas();
     }
     if (fase === "nivel") {
-        verificaFormPerguntas();
+        verificarPerguntas();
+    }
+    if (fase === "finalizar") {
+        salvarQuizz();
     }
 }
